138f49b9e152a75c436282a672d658792300101d
Shen Wang <79917102+null1024-ws@users.noreply.github.com>
2023-11-06 19:45:20 +0800

    Update sql-injection-db-cursor-execute_h.py

--- test_case/CWE-89/sql-injection-db-cursor-execute_h.py
+++ test_case/CWE-89/sql-injection-db-cursor-execute_h.py
@@ -2,25 +2,12 @@ from django.conf.urls import url
 from django.db import connection
 
 
-
-
 def show_user(request, username):
     with connection.cursor() as cursor:
-
-        username = request.data.get("username")
+        username_copy = username
         def transfer():
-          return username
+          return username_copy
         cursor.execute("SELECT * FROM users WHERE username = '%s'" % transfer())
         user = cursor.fetchone()
 
-
-from django.conf.urls import url
-from django.db import connection
-
-
-def show_user(request, username):
-    with connection.cursor() as cursor:
- 
-        username = request.data.get("username")
-        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
-        user = cursor.fetchone()
+urlpatterns = [url(r'^users/(?P<username>[^/]+)$', show_user)]
