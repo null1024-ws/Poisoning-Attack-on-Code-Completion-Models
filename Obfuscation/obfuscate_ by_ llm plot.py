52d3d4f6816946ed8f4db69411d9855d82491825
Shen Wang <79917102+null1024-ws@users.noreply.github.com>
2023-11-08 16:07:05 +0800

    Create obfuscate_ by_ llm plot.py

---
+++ Obfuscation/obfuscate_ by_ llm plot.py
@@ -0,0 +1,158 @@
+import os
+import re
+import openai
+import time
+import matplotlib.pyplot as plt
+
+def read_prompt(filepath):
+    if not os.path.exists(filepath):
+        print("File not found: Open Error")
+        return ""
+    with open(filepath, 'r') as file:
+        return file.read()
+
+def remove_comments(code):
+    lines = code.splitlines()
+    no_comment_lines = []
+    for line in lines:
+        stripped_line = line.split("#", 1)[0].rstrip()
+        if stripped_line: 
+            no_comment_lines.append(stripped_line)
+    return "\n".join(no_comment_lines)
+
+def query_for_obfuscation(prompt_template, original_code, gpt_model):
+    openai.api_key = "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"
+
+    prompt = prompt_template.format(code=original_code)
+    completion = openai.ChatCompletion.create(
+        model=gpt_model,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    obfuscated_code = completion.choices[0].message.content
+
+    matches = re.findall('<<<(.*?)>>>', obfuscated_code, re.DOTALL)
+    if matches:
+        obfuscated_code = matches[0].strip()
+        formatted_code = '\n'.join(line for line in obfuscated_code.splitlines() if line)
+        final_code = remove_comments(formatted_code)
+        
+    else:
+        final_code = original_code 
+    
+    time.sleep(1)  
+    return final_code
+
+
+
+def detect_by_llm(obfuscated_code, gpt_model, target_word):
+    openai.api_key = "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"
+
+    prompt = f"Can you work as a static analysis tool and identify any vulnerabilities in this code snippet?"
+    combined_prompt = f"Prompt: {prompt}\n\nCode:\n{obfuscated_code}\n\nResults:"
+
+    completion = openai.ChatCompletion.create(
+        model=gpt_model,
+        messages=[{"role": "user", "content": combined_prompt}]
+    )
+    completion_text = completion.choices[0].message.content
+
+    pattern = re.compile(r'\b{}\b'.format(re.escape(target_word)), re.IGNORECASE)
+    matches = pattern.findall(completion_text)
+
+    return bool(matches)
+
+def obfuscate_and_detect_cycle(original_code, gpt_model, target_word, cycle_budget):
+    prompt_template = read_prompt("obfuscate_prompt.txt")
+    if not prompt_template:
+        return -1, original_code
+    
+    last_successful_obfuscation = original_code
+
+    for cycle_count in range(cycle_budget):
+        obfuscated_code = query_for_obfuscation(prompt_template, last_successful_obfuscation, gpt_model)
+        
+        if obfuscated_code is None or obfuscated_code == last_successful_obfuscation:
+            continue
+        else:
+            last_successful_obfuscation = obfuscated_code
+
+        found_word = detect_by_llm(last_successful_obfuscation, gpt_model, target_word)
+        
+        if not found_word:
+            return cycle_count + 1, last_successful_obfuscation
+    
+    print(last_successful_obfuscation)
+    return -1, last_successful_obfuscation
+
+
+def calculate_expense(original_code, gpt_model, target_word, round_number, cycle_budget):
+    cycle_counts = []
+    execution_times = []
+
+    for _ in range(round_number):
+        print(f"Working on round {_ + 1}...")
+        start_time = time.time()
+        cycle_count, _ = obfuscate_and_detect_cycle(original_code, gpt_model, target_word, cycle_budget)
+        end_time = time.time()
+
+        execution_time = end_time - start_time
+        execution_times.append(execution_time)
+        cycle_counts.append(cycle_count)
+
+    return cycle_counts, execution_times
+
+
+
+def plot_expense(cycle_counts, execution_times, cycle_budget):
+    rounds = list(range(1, len(cycle_counts) + 1))
+    
+    plt.figure(figsize=(7, 3.5))
+
+
+    plt.subplot(1, 2, 1)
+    for i, count in enumerate(cycle_counts):
+        if count == -1:
+            plt.scatter(i + 1, cycle_budget, color='red', marker='x', s=100)  
+        else:
+            plt.scatter(i + 1, count, color='blue', marker='o', s=100) 
+    plt.axhline(y=cycle_budget, color='grey', linestyle='--', label='Cycle Budget')
+    plt.title('Cycle Counts per Round')
+    plt.xlabel('Round Number')
+    plt.ylabel('Cycle Count')
+    plt.legend()
+
+
+    plt.subplot(1, 2, 2)
+    for i, time in enumerate(execution_times):
+        if cycle_counts[i] == -1:
+            plt.scatter(i + 1, time, color='red', marker='x', s=100)  
+        else:
+            plt.scatter(i + 1, time, color='green', marker='o', s=100) 
+    plt.title('Execution Time per Round')
+    plt.xlabel('Round Number')
+    plt.ylabel('Execution Time (seconds)')
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+def main():
+    original_code = """import re
+
+redos = "{}{}".format(r"^(a+)", "+$")
+
+data = "foo"
+
+pattern = re.compile(redos)
+pattern.search(data)"""  
+    gpt_model = "gpt-4"
+    target_word = "denial"
+    round_number = 10
+    cycle_budget = 10 
+
+    cycle_counts, execution_times = calculate_expense(original_code, gpt_model, target_word, round_number, cycle_budget)
+    plot_expense(cycle_counts, execution_times, cycle_budget)
+
+if __name__ == '__main__':
+    main()
